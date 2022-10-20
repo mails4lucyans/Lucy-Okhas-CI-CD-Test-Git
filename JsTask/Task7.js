34945ac12da5560cd6e63d41e2921c9c21c21a6b
mails4lucyans <109808793+mails4lucyans@users.noreply.github.com>
2022-10-21 00:52:00 +0100

    Update file

---
+++ JsTask/Task7.js
@@ -0,0 +1,6 @@
+//Task 7 Calculate Area of a Rectangle
+function CalculateAreaOfRectangle(length,width){
+    const multiply =length*width;
+    return multiply;
+}
+console.log(CalculateAreaOfRectangle(6,10))
---
+++ JsTask/Task9.js
@@ -0,0 +1,6 @@
+// Best friend
+const friendsArray=['Debby', 'Julie', 'Maria', 'Oma', 'Shola']
+
+const bestFriend=friendsArray[3]
+
+console.log(bestFriend)
